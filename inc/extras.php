7c2fda310c3935f1ee9c06e314a6fcf2f11c7c1a
Rico Loschke <Rico Loschke>
2016-03-21 22:51:48 +0100

    # initial push via IDE

---
+++ inc/extras.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: losch
+ * Date: 21.03.2016
+ * Time: 21:31
+ */
---
+++ template-parts/content-none.php
@@ -0,0 +1,5 @@
+<!-- article -->
+<article>
+    <h2><?php _e( 'Sorry, nothing to display.', 'sevenx-theme' ); ?></h2>
+</article>
+<!-- /article -->
