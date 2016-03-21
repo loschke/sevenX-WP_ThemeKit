7e3647137fb4bc93279b5c8720304cec8b151475
Rico Loschke <Rico Loschke>
2016-03-21 23:02:22 +0100

    # add widget area to footer.php

--- footer.php
+++ footer.php
@@ -3,10 +3,16 @@
 
     <!-- copyright -->
     <p class="copyright">
-        &copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
-        <a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
+        &copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'sevenx-theme'); ?>
+        <a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//sevenx.de" title="Blank Wordpress Theme by sevenX">Blank Wordpress Theme by sevenX</a>.
     </p>
     <!-- /copyright -->
+
+    <!-- footer widget area -->
+    <div class="footer-widget">
+        <?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-widget')) ?>
+    </div>
+    <!-- /footer widget area -->
 
 </footer>
 <!-- /footer -->
